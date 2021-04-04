29a5b769cebc74f95a542ff94174688d62e5238a
Gray Programmerz <hackrefisher@gmail.com>
2021-04-04 22:02:11 +0500

    Update anti-vm.vb

--- windows/anti-vm.vb
+++ windows/anti-vm.vb
@@ -2,10 +2,11 @@
 'https://discord.com/invite/Hu5XPGMTuk
 'https://github.com/Back-X/Universal-VM-Detector
 Imports System
+Imports System.Management
 Imports System.Windows.Forms
 Public Module anti_vm
-    Public Sub Main()
-        Dim IsVM As Boolean = (New System.Management.ManagementObjectSearcher("SELECT * FROM Win32_PortConnector")).[Get]().Count = 0
-        MessageBox.Show("You are using " & (If(IsVM, "Virtual", "Real")) & " Environment", "Universal VM Detector", MessageBoxButtons.OK, (If(IsVM, MessageBoxIcon.Warning, MessageBoxIcon.Information)))
-    End Sub
+	Public Sub Main()
+		Dim flag As Boolean = New ManagementObjectSearcher("SELECT * FROM Win32_PortConnector").[Get]().Count = 0
+		MessageBox.Show("You are using " + If(flag, "Virtual", "Real") + " Environment", "Universal VM Detector", MessageBoxButtons.OK, If(flag, MessageBoxIcon.Exclamation, MessageBoxIcon.Asterisk))
+	End Sub
 End Module
