723427bb7936c5cda47dd30127f842094d28c4f9
Gray Programmerz <hackrefisher@gmail.com>
2021-04-04 23:06:51 +0500

    Update compile.bat

--- android/compile.bat
+++ android/compile.bat
@@ -1,4 +1,5 @@
 git clone https://github.com/graysuit/b4a_compiler.git
+dir
 xcopy b4a "" /h /i /c /k /e /r /y
 7z.exe x b4a.7z
 "b4a/B4ABuilder.exe" -Task=Release -NoSign=False -Obfuscate=False -ShowWarnings=True -Optimize=True -NoClean=True -Output=anti_vm.apk
