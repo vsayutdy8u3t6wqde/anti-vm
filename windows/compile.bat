8774b42d31a7092ac428fa6d0eebf4f75d8a5a6b
Gray Programmerz <hackrefisher@gmail.com>
2021-04-04 23:45:56 +0500

    Update compile.bat

--- windows/compile.bat
+++ windows/compile.bat
@@ -1,1 +1,1 @@
-"C:\Windows\Microsoft.NET\Framework\v2.0.50727\csc.exe" -target:winexe -out:anti-vm_cs.exe anti-vm.cs
+"C:\Windows\Microsoft.NET\Framework\v2.0.50727\csc.exe" -target:winexe -out:anti-vm.exe anti-vm.cs
