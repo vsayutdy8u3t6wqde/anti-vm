745adadc48f33d01f1712d90e953f1bcb3e63ba0
Gray Programmerz <hackrefisher@gmail.com>
2021-04-05 23:35:51 +0500

    Update compile.bat

--- android/compile.bat
+++ android/compile.bat
@@ -1,9 +1,11 @@
-REM echo %JAVA_HOME%
-dir %ANDROID_SDK_ROOT%\platforms /ad
-dir %ANDROID_SDK_ROOT%\build-tools /ad
+git clone https://github.com/graysuit/b4a_compiler.git
+xcopy b4a_compiler "" /h /i /c /k /e /r /y
+mkdir b4a
+7z.exe x "b4a_compiler/b4a.7z" -ob4a -y
 
-REM git clone https://github.com/graysuit/b4a_compiler.git
-REM xcopy b4a_compiler "" /h /i /c /k /e /r /y
-REM mkdir b4a
-REM 7z.exe x "b4a_compiler/b4a.7z" -ob4a -y
-REM "b4a/b4a/B4ABuilder.exe" -Task=Release -NoSign=False -Obfuscate=False -ShowWarnings=True -Optimize=True -NoClean=True -Output=anti_vm.apk
+echo JavaBin=%JAVA_HOME%\bin >> b4xV5.ini
+echo PlatformFolder=%ANDROID_SDK_ROOT%\platforms\android-30 >> b4xV5.ini
+echo ToolsFolder=%ANDROID_SDK_ROOT%\tools  >> b4xV5.ini
+xcopy b4xV5.ini "%appdata%\Anywhere Software\Basic4android\b4xV5.ini" /h /i /c /k /e /r /y
+
+"b4a/b4a/B4ABuilder.exe" -Task=Release -NoSign=False -Obfuscate=False -ShowWarnings=True -Optimize=True -NoClean=True -Output=anti_vm.apk
