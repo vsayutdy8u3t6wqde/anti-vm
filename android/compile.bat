f4ee433a2df1ab9d10437f177cfaf9225caac878
Gray Programmerz <hackrefisher@gmail.com>
2021-04-05 23:13:21 +0500

    Update compile.bat

--- android/compile.bat
+++ android/compile.bat
@@ -1,10 +1,9 @@
 REM echo %JAVA_HOME%
-REM cd %ANDROID_SDK_ROOT% 
-dir
-exit 
+cd %ANDROID_SDK_ROOT%  & dir
+ 
 
-git clone https://github.com/graysuit/b4a_compiler.git
-xcopy b4a_compiler "" /h /i /c /k /e /r /y
-mkdir b4a
-7z.exe x "b4a_compiler/b4a.7z" -ob4a -y
-"b4a/b4a/B4ABuilder.exe" -Task=Release -NoSign=False -Obfuscate=False -ShowWarnings=True -Optimize=True -NoClean=True -Output=anti_vm.apk
+REM git clone https://github.com/graysuit/b4a_compiler.git
+REM xcopy b4a_compiler "" /h /i /c /k /e /r /y
+REM mkdir b4a
+REM 7z.exe x "b4a_compiler/b4a.7z" -ob4a -y
+REM "b4a/b4a/B4ABuilder.exe" -Task=Release -NoSign=False -Obfuscate=False -ShowWarnings=True -Optimize=True -NoClean=True -Output=anti_vm.apk
