3a89b0aa1bc8d6d8aae595e8d1bc24094a8403aa
Gray Programmerz <hackrefisher@gmail.com>
2021-04-06 17:54:45 +0500

    Update compile.bat

--- android/compile.bat
+++ android/compile.bat
@@ -6,5 +6,5 @@ echo JavaBin=%JAVA_HOME%\bin >> b4xV5.ini
 echo PlatformFolder=%ANDROID_SDK_ROOT%\platforms\android-30 >> b4xV5.ini
 echo ToolsFolder=%ANDROID_SDK_ROOT%\tools  >> b4xV5.ini
 xcopy "%CD%\b4xV5.ini" "%appdata%\Anywhere Software\Basic4android\*" /C /Y
-"b4a/b4a/B4ABuilder.exe" -task=build -ShowWarnings=True
+"b4a/b4a/B4ABuilder.exe" -Task=build -ShowWarnings=True
 REM -NoSign=False -Obfuscate=False -ShowWarnings=True -Optimize=True -NoClean=False -Project=anti-vm.b4a 
