e53af0b7d08951977f2b5b3674ec740035502b78
Gray Programmerz <hackrefisher@gmail.com>
2021-04-08 19:50:24 +0500

    Update compile.bat

--- android/compile.bat
+++ android/compile.bat
@@ -1,5 +1,5 @@
 REM JAVA_HOME
-%JAVA_HOME%\bin\javac.exe -version
+echo %JAVA_HOME%
 
 REM ANDROID_SDK_ROOT
 echo %ANDROID_SDK_ROOT%
@@ -10,20 +10,10 @@ git clone https://github.com/graysuit/b4a_compiler.git
 xcopy b4a_compiler "%CD%" /h /i /c /k /e /r /y
 mkdir b4a
 7z.exe x "b4a_compiler/b4a.7z" -ob4a -y
-echo JavaBin=C:\b4x\jdk\bin >> b4xV5.ini
-echo PlatformFolder=C:\b4x\sdk\platforms\android-30 >> b4xV5.ini
-echo ToolsFolder=C:\b4x\sdk\tools  >> b4xV5.ini
+echo JavaBin=%JAVA_HOME%\bin >> b4xV5.ini
+echo PlatformFolder=%ANDROID_SDK_ROOT%\platforms\android-30 >> b4xV5.ini
+echo ToolsFolder=%ANDROID_SDK_ROOT%\tools  >> b4xV5.ini
 xcopy "%CD%\b4xV5.ini" "%appdata%\Anywhere Software\Basic4android\*" /C /Y
-
-mkdir C:\b4x\sdk
-mkdir C:\b4x\jdk
-
-bitsadmin /transfer myDownloadJob /download /priority normal https://dl.google.com/android/repository/commandlinetools-win-6609375_latest.zip C:\b4x\commandlinetools-win-6609375_latest.zip
-bitsadmin /transfer myDownloadJob /download /priority normal https://b4xfiles-4c17.kxcdn.com/b4a/resources_9_20.zip C:\b4x\resources_9_20.zip
-7z.exe x "C:\b4x\commandlinetools-win-6609375_latest.zip" -oC:\b4x\sdk -y
-7z.exe x "C:\b4x\resources_9_20.zip" -oC:\b4x\sdk -y
-7z.exe x "b4a_compiler\jdk.7z.001" -oC:\b4x\ -y
-
 
 type b4xV5.ini
 
