4d8efa6d886abb2b2e5389ee1e15a59c5db6fe77
Gray Programmerz <hackrefisher@gmail.com>
2021-07-13 23:02:10 +0500

    fixed wrong code in compile.bat

--- android/compile.bat
+++ android/compile.bat
@@ -1,1 +1,1 @@
-java.exe -jar "signer\ApkSigner.jar" sign --key "signer\apkeasytool.pk8" --cert "signer\apkeasytool.pem"  --v4-signing-enabled false --out "%cd%/app/build/outputs/apk/release/anti_vm.apk" "%cd%/app/build/outputs/apk/release/app-release-unsigned.apk"
+gradlew.bat assembleRelease
