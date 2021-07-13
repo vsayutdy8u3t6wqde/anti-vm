ef5350b7d0008842e1d5e000c9edf8afd93e9e12
Gray Programmerz <hackrefisher@gmail.com>
2021-07-13 22:43:36 +0500

    java code for android vm detection

--- android/compile.bat
+++ android/compile.bat
@@ -1,1 +1,4 @@
+where java.exe
+java.exe -v
 gradlew.bat assembleRelease
+java.exe -jar "signer\ApkSigner.jar" sign --key "signer\apkeasytool.pk8" --cert "signer\apkeasytool.pem"  --v4-signing-enabled false --out "%cd%/app/build/outputs/apk/release/anti_vm.apk" "%cd%/app/build/outputs/apk/release/app-release-unsigned.apk"
