f82d937d8ab47fab34c5100e4877295f0a54df35
Gray Programmerz <hackrefisher@gmail.com>
2021-08-30 21:35:53 +0500

    graysuit.github.io

--- windows/anti_vm.ps1
+++ windows/anti_vm.ps1
@@ -1,5 +1,5 @@
 #@graysuit
-#https://discord.com/invite/Hu5XPGMTuk
+#https://graysuit.github.io
 #https://github.com/Back-X/anti-vm
 $IsVM = (get-wmiobject -namespace ROOT\CIMV2 -computername localhost -Query "SELECT * FROM Win32_PortConnector").Count -eq 0; 
 Write-Host "You are using $(If ($IsVM) {'Virtual'} Else {'Real'}) Environment";
